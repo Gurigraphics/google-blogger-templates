3404303f2c7ecfcc50d49a11cb63f5c1fa971ddb
Me <me@eblan.(none)>
2013-01-25 16:49:23 +0400

    *

---
+++ kykyev2/config.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+require 'css-grids'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+
+line_comments = false
